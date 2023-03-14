db1abc7c8f30734b79c782eaa32081909dd6838d
PabloSebastian92 <sebaruso92@gmail.com>
2023-03-13 22:09:16 -0300

    commit

---
+++ HacerUnAsado.js
@@ -0,0 +1,5 @@
+console.log("PASO 1:Prender el fuego ");
+console.log("PASO 2:Sasonar la carne");
+console.log("PASO 3:Poner la carne en el fuego");
+console.log("PASO 4:Pasada 1 hora darla vuelta");
+console.log("PASO 5:Retirar la carne del fuego y disfrutar!");
